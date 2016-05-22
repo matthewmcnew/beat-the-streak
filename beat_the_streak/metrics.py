4ae717a85ec140a7b3301598873d28c3b674ef93
Matthew McNew <matthewmcnew@gmail.com>
2016-05-22 18:16:52 -0500

    Update Metrics to be a callable class that recieves the game dates
    + pandas is used to join the game dates to game dates to the feature vector

--- beat_the_streak/metrics.py
+++ beat_the_streak/metrics.py
@@ -1,17 +1,24 @@
 import numpy as np
+import pandas as pd
 
 
-def best_pick_for_each_day_got_hit_percent(clf, X, y):
-    df = X.copy()
-    df['got_hit'] = y
-    df['prob_of_hit'] = np.array(clf.predict_proba(X)).T[1]
+class BestPickForEachDayGotHitPercent(object):
+    def __init__(self, game_dates):
+        self.game_dates = game_dates
 
-    days_with_hit = 0
-    for date in df.game_date.unique():
+    def __call__(self, clf, X, y, *args, **kwargs):
+        df = X.copy()
+        df['prob_of_hit'] = np.array(clf.predict_proba(X)).T[1]
+        df['got_hit'] = [got_hit for got_hit in y]
+        # noinspection PyTypeChecker
+        df = pd.concat([df, self.game_dates], join='inner', axis=1)
 
-        date_frame = df[df.game_date == date]
+        days_with_hit = 0
+        for date in df.game_date.unique():
+            date_frame = df[df.game_date == date]
 
-        if date_frame.sort_values('prob_of_hit', ascending=False).iloc[0]['got_hit']:
-            days_with_hit += 1
+            if date_frame.sort_values('prob_of_hit', ascending=False).iloc[0]['got_hit']:
+                days_with_hit += 1
+        print len(df.game_date.unique())
 
-    return float(days_with_hit)/len(df.game_date.unique())
+        return float(days_with_hit) / len(df.game_date.unique())
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,15 +1,18 @@
 import unittest
 
 import pandas as pd
-from beat_the_streak.metrics import best_pick_for_each_day_got_hit_percent
+from beat_the_streak.metrics import BestPickForEachDayGotHitPercent
+
+CHOICES = pd.DataFrame({
+    'game_date': ['12-12-10', '12-12-10', '12-12-11', '12-12-11'],
+    'some_feature': [1, 2, 3, 4],
+    'is_first_in_group': [1, 0, 1, 0]},
+    index=['a', 'b', 'c', 'd'])
+X = CHOICES[['some_feature', 'is_first_in_group']]
 
 
 class BestPickForEachDayGotHitPercentTest(unittest.TestCase):
-
     def test_returns_the_percent_of_days_where_models_highest_prob_got_a_hit(self):
-
-        X = pd.DataFrame({'game_date': ['12-12-10', '12-12-10', '12-12-11', '12-12-11']})
-
         class StubClassifier(object):
             def predict_proba(self, x_to_predict):
                 assert x_to_predict is X
@@ -18,14 +21,11 @@ class BestPickForEachDayGotHitPercentTest(unittest.TestCase):
 
         y = pd.Series([1, 0, 0, 0])
 
-        score = best_pick_for_each_day_got_hit_percent(StubClassifier(), X, y)
+        score = BestPickForEachDayGotHitPercent(CHOICES.game_date)(StubClassifier(), X, y)
 
         assert score == .5
 
     def test_handles_when_not_highest_prob_got_hit(self):
-
-        X = pd.DataFrame({'game_date': ['12-12-10', '12-12-10', '12-12-11', '12-12-11']})
-
         class StubClassifier(object):
             def predict_proba(self, x_to_predict):
                 assert x_to_predict is X
@@ -34,7 +34,17 @@ class BestPickForEachDayGotHitPercentTest(unittest.TestCase):
 
         y = pd.Series([1, 0, 1, 0])
 
-        score = best_pick_for_each_day_got_hit_percent(StubClassifier(), X, y)
+        score = BestPickForEachDayGotHitPercent(CHOICES.game_date)(StubClassifier(), X, y)
 
         assert score == 0
 
+    def test_uses_index_to_match_dates(self):
+        class StubClassifier(object):
+            def predict_proba(self, x_to_predict):
+                return [[.8, .1], [.2, .8]]
+
+        y = pd.Series([1, 0])
+
+        score = BestPickForEachDayGotHitPercent(CHOICES.game_date)(StubClassifier(), X[X.is_first_in_group == 1], y)
+
+        assert score == .5
